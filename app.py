1701dd7ce042df32a68ba1c1296b5ba25a1e6d5c
Tran Cong Duy <congduytran12@gmail.com>
2025-11-20 16:54:14 +0700

    Enable sharing in demo launch

--- app.py
+++ app.py
@@ -139,4 +139,4 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     )
 
 if __name__ == "__main__":
-    demo.launch(debug=True, server_name="localhost")
+    demo.launch(debug=True, share=True)
